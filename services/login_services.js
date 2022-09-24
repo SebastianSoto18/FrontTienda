dd3dfc4283a6df05989a29c1611695ebf17c1b65
Sebastian18 <atlasjuanse12@gmail.com>
2022-09-24 17:54:27 -0500

    arreglando alerta de error en el login

--- services/login_services.js
+++ services/login_services.js
@@ -2,7 +2,10 @@
 const login=  (email, password) => fetch("https://tiendarest.herokuapp.com/login",
         {method: "POST", 
         headers:{"Content-type":"application/json"},
-        body: JSON.stringify({email: email,password: password})});
+        body: JSON.stringify({email: email,password: password})}).then
+        ((response) => {
+                return response.status;
+        });
 
 
 const register = (email, password, name, phone) => fetch("https://tiendarest.herokuapp.com/users",
