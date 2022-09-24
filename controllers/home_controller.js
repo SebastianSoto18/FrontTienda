d759705cf1883c6f7230d0b9996bf3dae820491b
Sebastian18 <atlasjuanse12@gmail.com>
2022-09-24 16:56:10 -0500

    creando modulo para la nav

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,7 @@
-import { user_service } from "../services/user_services.js";
+
 import { products_service } from "../services/product_services.js";
 
-function decoteJWT(token) {
-    const base64Url = token.split(".")[1];
-    const base64 = base64Url.replace("-", "+").replace("_", "/");
-    return JSON.parse(window.atob(base64));
-}
-
-
-const data = await user_service.getuser(decoteJWT(document.cookie.split("=")[1].substring(0, document.cookie.split("=")[1].length - 1))["user_id"]).catch((err) => { window.location.href = "https://sebastiansoto18.github.io/FrontTienda/index.html"; return });
-const user = await data.json();
-
-const {name, email} = user;
-
-export const data_user = {name,email};
-
-document.getElementById("user_name").innerHTML = name;
-document.getElementById("user_email").innerHTML = email;
-
-const logout = document.getElementById("salir");
-
-logout.addEventListener("click", () => {
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    });
-
-
-
-
 const lista=document.getElementById("productos"); 
-
-
-const products = await products_service.getproducts().then(response => response.json());
 
 function createProduct(product) {
     const li = document.createElement("li");
@@ -49,6 +20,9 @@ function createProduct(product) {
 
     return li;
 }
+
+const products = await products_service.getproducts().then(response => response.json());
+
 
 products.forEach(product => {
     lista.appendChild( createProduct(product) );
